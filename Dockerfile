5463b1d2d371245c1390aa0cffaf2f19e86b0b48
Paul Bridi <paulbridi@gmail.com>
2023-03-14 12:41:16 +0000

    remove .env from dockerfile, call api from app

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,13 @@ FROM python:3.10.6-buster
 WORKDIR /server
 
 COPY requirements_api.txt ./requirements_api.txt
-COPY .env ./.env
 
 RUN pip install --upgrade pip \
   && pip install -r requirements_api.txt \
   && apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
 
 COPY packages ./packages
+COPY models ./models
 COPY docker_data ./docker_data
 
 CMD uvicorn packages.api:app --host 0.0.0.0 --port $PORT
--- packages/api.py
+++ packages/api.py
@@ -6,6 +6,7 @@ import pandas as pd
 import cv2
 import io
 from nlp_main import front_end_display
+from cnn_model import make_predictions
 
 app = FastAPI()
 
@@ -24,6 +25,33 @@ def index():
     return {'status': True}
 
 
+# @app.post('/upload_image')
+# async def receive_image(img: UploadFile=File(...)):
+#     ### Receiving and decoding the image
+#     contents = await img.read()
+
+#     nparr = np.fromstring(contents, np.uint8)
+#     cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # type(cv2_img) => numpy.ndarray
+
+#     ### Do cool stuff with your image.... For example face detection
+#     annotated_img = annotate_face(cv2_img)
+
+#     ### Encoding and responding with the image
+#     im = cv2.imencode('.png', annotated_img)[1] # extension depends on which format is sent from Streamlit
+#     return Response(content=im.tobytes(), media_type="image/png")
+
+@app.get("/predict")
+def predict(ingredients: list, *preferences):
+    df_path = "../docker_data/final_nlp_model_cit.model"
+    model_path = "../docker_data/final_cleaned_recipes_dataset.pkl"
+    predictions = nlp_main.front_end_display(ingredients, preferences, df_path, model_path)
+    return {"predictions": predictions}
+
+@app.post("/predict_ingredient")
+def predict_ingredient():
+    return make_predictions()
+
+
 @app.post('/upload_image')
 async def receive_image(img: UploadFile=File(...)):
     ### Receiving and decoding the image
@@ -33,18 +61,7 @@ async def receive_image(img: UploadFile=File(...)):
     cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # type(cv2_img) => numpy.ndarray
 
     ### Do cool stuff with your image.... For example face detection
-    annotated_img = annotate_face(cv2_img)
+    ingredients = make_predictions(cv2_img)
 
     ### Encoding and responding with the image
-    im = cv2.imencode('.png', annotated_img)[1] # extension depends on which format is sent from Streamlit
-    return Response(content=im.tobytes(), media_type="image/png")
-
-
-@app.get("/predict")
-def predict(ingredients: list, *preferences):
-    df_path = "../docker_data/final_nlp_model_cit.model"
-    model_path = "../docker_data/final_cleaned_recipes_dataset.pkl"
-    predictions = nlp_main.front_end_display(ingredients, preferences, df_path, model_path)
-    return {"predictions": predictions}
-
-
+    return ingredients
